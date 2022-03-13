6432a1c365bc5b12548b9bd5f77952e0684f871a
Jakub Pelz <JakubMac@Jakub-MacBook-Air.local>
2022-03-13 07:25:23 +0100

    Bug fix

--- src/items/items.controller.ts
+++ src/items/items.controller.ts
@@ -13,15 +13,30 @@ import { Item } from './interfaces/item.interface';
 
 @Controller('items')
 export class ItemsController {
-  /* constructor(private readonly itemsService: ItemsService) {}
+  constructor(private readonly itemsService: ItemsService) {}
 
   @Get()
-  findAll(): Item[] {
+  findAll(): Promise<Item[]> {
     return this.itemsService.findAll();
   }
 
   @Get(':id')
-  findOne(@Param('id') id): string {
-    return `${id} item`;
-  } */
+  findOne(@Param('id') id): Promise<Item> {
+    return this.itemsService.findOne(id);
+  }
+
+  @Post()
+  create(@Body() createItemDto: CreateItemDto): Promise<Item> {
+    return this.itemsService.create(createItemDto);
+  }
+
+  @Delete(':id')
+  delete(@Param('id') id): Promise<Item> {
+    return this.itemsService.delete(id);
+  }
+
+  @Put(':id')
+  update(@Body() updateItemDto: CreateItemDto, @Param('id') id): Promise<Item> {
+    return this.itemsService.update(id, updateItemDto);
+  }
 }
